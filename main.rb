038f7d5115a59b8aacaf15ecd2dab03972441a94
andresporras3423 <andresporras3423@gmail.com>
2020-02-18 10:16:08 -0500

    added bubble_sort_by method

--- main.rb
+++ main.rb
@@ -15,4 +15,24 @@ def bubble_sort(ar)
   return ar
 end
 
-puts bubble_sort([4,3,78,2,0,2])
+def bubble_sort_by(ar)
+  i=0
+  while i<(ar.length)
+    j=1
+    while j<(ar.length-i)
+      if 0<(yield ar[j-1], ar[j])
+        temp=ar[j-1]
+        ar[j-1]=ar[j]
+        ar[j]=temp
+      end
+      j+=1
+    end
+    i+=1
+  end
+  puts ar
+end
+
+# puts bubble_sort([4,3,78,2,0,2])
+# bubble_sort_by(["hi","hello","hey"]) do |left,right|
+#   left.length - right.length
+# end
