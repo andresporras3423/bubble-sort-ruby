28dade8bb2e7d0d84c02fdc4f6baba42b47f165e
abcussi <52175588+abcussi@users.noreply.github.com>
2020-02-18 11:56:43 -0400

    STICKLER FIXS

--- main.rb
+++ main.rb
@@ -1,35 +1,35 @@
 def bubble_sort(arr)
   i = 0
-  while i < (arr.length)
+  while i < arr.length
     j = 1
-    while j < (arr.length-i)
-      if arr[j-1] > arr[j]
-        temp=arr[j-1]
-        arr[j-1] = arr[j]
+    while j < arr.length - i
+      if arr[j - 1] > arr[j]
+        temp=arr[j - 1]
+        arr[j - 1] = arr[j]
         arr[j] = temp
       end
       j += 1
     end
     i += 1
   end
-  return "["+arr.join(",")+"]"
+  return "[" + arr.join(",") + "]"
 end
 
 def bubble_sort_by(arr)
   i = 0
-  while i < (arr.length)
+  while i < arr.length
     j = 1
-    while j < (arr.length-i)
-      if 0 < (yield arr[j-1], arr[j])
-        temp = arr[j-1]
-        arr[j-1] = arr[j]
+    while j < arr.length - i
+      if 0 < (yield arr[j - 1], arr[j])
+        temp = arr[j - 1]
+        arr[j - 1] = arr[j]
         arr[j] = temp
       end
       j += 1
     end
     i += 1
   end
-  puts "["+'"'+arr.join('","')+'"'+"]"
+  puts "[" + '"' + arr.join('","') + '"' + "]"
 end
 
 puts bubble_sort([4,3,78,2,0,2])
